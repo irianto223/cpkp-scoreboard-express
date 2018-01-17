6e319290189e99cbfec100a9747ffad670c1c8ae
irianto <irianto223@gmail.com>
2018-01-17 22:39:16 +0700

    change arrow function

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const userController = require('../controllers/userController')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res) => {
   res.send({ message: 'it works! '});
 });
 
